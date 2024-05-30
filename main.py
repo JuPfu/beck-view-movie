59853856b2e092817cd442b9a59b78135994462b
Juergen Pfundt <juergen.pfundt@gmail.com>
2024-05-30 16:33:05 +0200

    Added handler for SIGINT signal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import signal
 from argparse import Namespace
 from multiprocessing import freeze_support
+from types import FrameType
 
 from CommandLineParser import CommandLineParser
 from createVideo import GenerateVideo
@@ -8,11 +10,19 @@ from createVideo import GenerateVideo
 def main():
     freeze_support()
 
+    signal.signal(signal.SIGINT, sigint_handler)
+
     args: Namespace = CommandLineParser().parse_args()
 
     generate_video = GenerateVideo(args.path, args.opath, args.name, args.fps, args.batch_size)
 
     generate_video.assemble_video(str(args.path / "frame*.png"))
+
+
+def sigint_handler(signum: int, frame: FrameType | None):
+    signame = signal.Signals(signum).name
+    print(f"\nProgram terminated by signal '{signame}' at {frame}")
+    exit(1)
 
 
 # Press the green button in the gutter to run the script.
