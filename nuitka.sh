eafc2d7f770cc6fcb358072869db71ebe4c6fdb8
Juergen Pfundt <juergen.pfundt@gmail.com>
2025-07-28 21:18:44 +0200

    added --static-libpython=no to nuitka.sh

--- nuitka.sh
+++ nuitka.sh
@@ -1,1 +1,1 @@
-python3 -m nuitka  --product-name="beck-view-movie" --standalone --onefile  --macos-app-icon=beck-view-movie.png -o "beck-view-movie" main.py
+python3 -m nuitka  --product-name="beck-view-movie" --standalone --onefile  --macos-app-icon=beck-view-movie.png --static-libpython=no -o "beck-view-movie" main.py
