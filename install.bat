040229764980aa8fe883c29dd29afb5f038d8f40
Juergen Pfundt <juergen.pfundt@gmail.com>
2025-08-07 13:18:50 +0200

    adapted install.bat to create an executable utilising Cython and pyinstaller

--- install.bat
+++ install.bat
@@ -1,1 +1,9 @@
-pyinstaller --onefile --name "beck-view-movie-jp.exe" --add-binary "C:/Users/Peter lokal/PycharmProjects/beck-view-movie\build\lib.win-amd64-cpython-313\createVideo.cp313-win_amd64.pyd:build\lib.win-amd64-cpython-313" main.py
+rmdir build
+rmdir dist
+del *.c *.pyd
+python setup.py build_ext --inplace
+mkdir dist
+move build\lib.win-amd64-cpython-313\*.pyd dist
+pyinstaller beck-view-movie.spec --noconfirm
+move /y dist\beck-view-movie-bundle\beck-view-movie.exe .
+echo "Executable `beck-view-movie.exe` ready for use in %CD%"
