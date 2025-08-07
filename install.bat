c4352aefb2d39190a9ad6958d911f7f6c49a62b9
jp <juergen.pfundt@gmail.com>
2025-08-07 13:27:24 +0200

    small adjustment to build skript

--- install.bat
+++ install.bat
@@ -5,5 +5,5 @@ python setup.py build_ext --inplace
 mkdir dist
 move build\lib.win-amd64-cpython-313\*.pyd dist
 pyinstaller beck-view-movie.spec --noconfirm
-move /y dist\beck-view-movie-bundle\beck-view-movie.exe .
+move /y dist\beck-view-movie.exe .
 echo "Executable `beck-view-movie.exe` ready for use in %CD%"
