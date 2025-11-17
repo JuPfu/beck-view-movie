b1edb0bb595dca098405e29f432ba8758623128c
Juergen Pfundt <juergen.pfundt@gmail.com>
2025-11-17 19:23:14 +0100

    added asf codec

--- CommandLineParser.py
+++ CommandLineParser.py
@@ -97,7 +97,7 @@ class CommandLineParser:
             '-c', '--codec',
             dest="codec",
             type=str,
-            choices=["avc1", "h263", "h264", "h265", "mjpg", "mpv4", "wmv", "xvid"],
+            choices=["asf", "avc1", "h263", "h264", "h265", "mjpg", "mpv4", "wmv", "xvid"],
             nargs='?',
             default="avc1",  # which is a H.264 encoder
             help='Supported codecs. See https://gist.github.com/takuma7/44f9ecb028ff00e2132e for more information.'
